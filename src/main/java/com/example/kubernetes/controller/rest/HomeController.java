c0e17646d85b04e888c50e3327247e2da584592e
mertcakmak2 <mertcakmak2@gmail.com>
2021-11-19 13:31:31 +0300

    Spring app deployment with Kubernetes

---
+++ src/main/java/com/example/kubernetes/controller/rest/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.kubernetes.controller.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello from kubernetes";
+    }
+
+}
